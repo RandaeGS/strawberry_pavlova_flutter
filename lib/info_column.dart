d772b8ddded8a8c70dca8d865c391d7fc63fe26d
RandaeGS <garciasanchezrd@gmail.com>
2024-10-22 14:14:42 -0400

    Crear archivo para widget de informacion

---
+++ lib/info_column.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InfoColumn extends StatelessWidget {
+  const InfoColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
