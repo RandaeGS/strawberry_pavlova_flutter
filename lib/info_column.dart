50423336ce0614309888f212bac545c9af227e43
RandaeGS <garciasanchezrd@gmail.com>
2024-10-22 18:50:55 -0400

    Anadir informacion de preparacion

--- lib/info_column.dart
+++ lib/info_column.dart
@@ -28,6 +28,48 @@ final reviewInfo = Container(
   ),
 );
 
+const descTextStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.5,
+  fontSize: 18,
+  height: 2,
+);
+
+final iconList = DefaultTextStyle.merge(
+  style: descTextStyle,
+  child: Container(
+    padding: const EdgeInsets.all(20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Icon(Icons.kitchen, color: Colors.green[500]),
+            const Text('PREP:'),
+            const Text('25 min'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.timer, color: Colors.green[500]),
+            const Text('COOK:'),
+            const Text('1 hr'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.restaurant, color: Colors.green[500]),
+            const Text('FEEDS:'),
+            const Text('4-6'),
+          ],
+        ),
+      ],
+    ),
+  ),
+);
+
 class InfoColumn extends StatelessWidget {
   static const String description = "Pavlova is a meringue-based dessert named after the"
       " Russian ballerina Anna pavlova. Pavlova features a crisp crust and soft, light inside, topped"
@@ -71,9 +113,9 @@ class InfoColumn extends StatelessWidget {
             reviewInfo,
           ],
         ),
-        const Row(
+        Row(
           children: [
-
+            iconList,
           ],
         )
       ],
