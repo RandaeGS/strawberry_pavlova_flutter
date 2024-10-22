88ec65391db5dffeb13ef3ce76af2c0a6a6f6116
RandaeGS <garciasanchezrd@gmail.com>
2024-10-22 16:38:51 -0400

    Agregar y estilizar el texto de la informacion

--- lib/info_column.dart
+++ lib/info_column.dart
@@ -29,6 +29,9 @@ final reviewInfo = Container(
 );
 
 class InfoColumn extends StatelessWidget {
+  static const String description = "Pavlova is a meringue-based dessert named after the"
+      " Russian ballerina Anna pavlova. Pavlova features a crisp crust and soft, light inside, topped"
+      " with fruit and whipped cream.";
 
   const InfoColumn({super.key});
 
@@ -37,8 +40,30 @@ class InfoColumn extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        const Text("hola"),
-        const Text("Hola"),
+        const Text(
+          "Strawberry Pavlova",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w800,
+            fontFamily: 'Roboto',
+            letterSpacing: 0.5,
+            fontSize: 20,
+          ),
+        ),
+
+        const SizedBox(
+          width: 250,
+          child: Text(
+            description,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
